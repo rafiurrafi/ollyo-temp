30b711f07bac662e745db9b94bff62beb8666dd5
Rafiur Rahman <rrfoundary@gmail.com>
2023-11-04 01:58:13 +0600

    added selection and dragging features

--- src/App.jsx
+++ src/App.jsx
@@ -3,61 +3,115 @@ import "./app.scss";
 import { closestCenter, DndContext } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 import {
-  arrayMove,
-  horizontalListSortingStrategy,
   SortableContext,
   useSortable,
+  rectSortingStrategy,
+  arrayMove,
+  rectSwappingStrategy,
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
-import { useState } from "react";
-function SortableItem({ image }) {
+import { useEffect, useState } from "react";
+
+const CheckboxButton = ({ handleCheckboxClicked, id }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxClick = () => {
+    setIsChecked(!isChecked);
+    handleCheckboxClicked(id);
+  };
+
+  return (
+    <button
+      className={`custom-checkbox ${isChecked ? "checked" : ""}`}
+      onMouseDown={() => {
+        handleCheckboxClick();
+      }}
+    ></button>
+  );
+};
+
+function SortableItem({ image, handleCheckboxClick }) {
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: image.id });
   const style = { transform: CSS.Transform.toString(transform), transition };
   return (
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <img src={image.src} />
+    <div
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className={`gallery-img ${
+        image.isFeature ? "gallery-img-featured" : ""
+      } `}
+    >
+      <div
+        style={{ position: "relative", display: "inline-block" }}
+        onClick={() => console.log("Clicked")}
+      >
+        <img src={image.src} alt={`Image ${image.id}`} />
+
+        <CheckboxButton
+          handleCheckboxClicked={handleCheckboxClick}
+          id={image.id}
+        />
+      </div>
     </div>
   );
 }
 
 const App = () => {
   const [images, setImages] = useState(fakeImages);
-
   function handleDragEnd(e) {
-    // const { active, over } = e;
-    // if (active.id !== over.id) {
-    //   console.log("MEet");
-    //   setImages((items) => {
-    //     const activeIndex = items.findIndex((item) => item.id === active.id);
-    //     const overIndex = items.findIndex((item) => item.id === over.id);
-    //     console.log(activeIndex, overIndex);
-    //     return arrayMove(items, activeIndex, overIndex);
-    //   });
-    // }
     const { active, over } = e;
     if (active.id === over.id) {
       return;
     }
     const activeIndex = images.findIndex((image) => image.id === active.id);
     const overIndex = images.findIndex((image) => image.id === over.id);
-    const newItems = [...images];
+    let newItems = [...images];
     const [removed] = newItems.splice(activeIndex, 1);
     newItems.splice(overIndex, 0, removed);
+    // let newItems = arrayMove(images, activeIndex, overIndex);
+
+    newItems = newItems.map((img, index) =>
+      index === 0 ? { ...img, isFeature: true } : { ...img, isFeature: false }
+    );
 
     setImages(newItems);
   }
-  console.log(images);
+
+  const handleCheckboxClick = (id) => {
+    console.log("checked");
+    const updatedImages = images.map((image) =>
+      image.id === id ? { ...image, selected: !image.selected } : image
+    );
+    setImages(updatedImages);
+  };
+
+  const handleDeleteSelected = () => {
+    const remainingImages = images.filter((image) => !image.selected);
+    setImages(remainingImages);
+  };
+
   return (
-    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-      <SortableContext items={images} strategy={horizontalListSortingStrategy}>
-        <div className="image-container">
-          {images.map((img) => (
-            <SortableItem key={img.id} image={img} />
-          ))}
-        </div>
-      </SortableContext>
-    </DndContext>
+    <div className="container">
+      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+        <SortableContext items={images} strategy={rectSwappingStrategy}>
+          <div className="image-container">
+            {images.map((img) => (
+              <SortableItem
+                key={img.id}
+                image={img}
+                handleCheckboxClick={handleCheckboxClick}
+              />
+            ))}
+          </div>
+        </SortableContext>
+      </DndContext>
+      <button onClick={handleDeleteSelected} style={{ marginTop: 100 }}>
+        Delete Selected
+      </button>
+    </div>
   );
 };
 
