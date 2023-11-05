bac6ca1996664c1cd7964e530434b71d3b074e98
Rafiur Rahman <rrfoundary@gmail.com>
2023-11-05 19:13:06 +0600

    added sensor for keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { fakeImages } from "./service/fakeImageService";
 import "./app.scss";
-import { closestCenter, DndContext } from "@dnd-kit/core";
-import { SortableContext, rectSwappingStrategy } from "@dnd-kit/sortable";
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  rectSwappingStrategy,
+  sortableKeyboardCoordinates,
+} from "@dnd-kit/sortable";
 import { useState } from "react";
 import imgPlaceholder from "./img/image.png";
 import SortableItem from "./sortable-item/sortable-item.component";
 
 const App = () => {
   const [images, setImages] = useState(fakeImages);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
   function handleDragEnd(e) {
     const { active, over } = e;
     if (active.id === over.id) {
@@ -27,7 +44,6 @@ const App = () => {
   }
 
   const handleCheckboxClick = (id) => {
-    console.log("checked");
     const updatedImages = images.map((image) =>
       image.id === id ? { ...image, selected: !image.selected } : image
     );
@@ -67,7 +83,11 @@ const App = () => {
           ) : null}
         </div>
       </div>
-      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
         <SortableContext items={images} strategy={rectSwappingStrategy}>
           <div className="image-container">
             {images.map((img) => (
